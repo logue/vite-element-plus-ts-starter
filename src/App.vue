893d634fd7d381db61145b91c5c5498154347acb
Logue <logue@hotmail.co.jp>
2023-05-15 11:51:19 +0900

    Fixed right content scroll bar.

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <el-header class="header fixed top-0 z-12 w-screen px-0">
       <base-header />
     </el-header>
-    <div class="h-screen flex container">
-      <el-aside width="auto" class="sticky">
+    <div class="content h-screen w-screen flex">
+      <el-aside width="auto" class="sticky shrink-0">
         <base-side />
       </el-aside>
-      <div>
+      <div class="grow-1">
         <el-scrollbar>
           <el-main>
             <transition name="el-fade-in-linear">
@@ -21,14 +21,14 @@
 </template>
 
 <style scoped>
-.container {
-  margin-top: 60px;
-  height: calc(100vh - 60px);
-}
-
 .header {
   background-image: radial-gradient(transparent 1px, var(--ep-bg-color) 1px);
   background-size: 4px 4px;
   backdrop-filter: saturate(50%) blur(4px);
+}
+
+.content {
+  margin-top: 60px;
+  height: calc(100vh - 60px);
 }
 </style>
