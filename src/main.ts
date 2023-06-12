01bb83f2086dc95293926bd0da41e6b60ab1737e
Logue <logue@hotmail.co.jp>
2023-06-12 15:59:56 +0900

    Update Element PLus and yarn. Update dependencies. Rename stores to store. Optimize eslint rules.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
 /**
  * Vue3 Main script
  */
+import store from '@/store';
 import { createApp } from 'vue';
 
 import App from '@/App.vue';
 import ElementPlus from '@/plugins/element-plus';
 import router from '@/router';
-import stores from '@/stores';
 
 /** Register Vue */
 const vue = createApp(App);
 vue.use(router);
-vue.use(stores);
+vue.use(store);
 vue.use(ElementPlus);
 
 // Run!
 router
   .isReady()
-  .then(() => {
-    vue.mount('#app');
-  })
-  .catch(e => {
-    console.error(e);
-  });
+  .then(() => vue.mount('#app'))
+  .catch(e => console.error(e));
