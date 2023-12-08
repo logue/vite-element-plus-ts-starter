2d4db4550c9c2957d146602d027fc80c9895d3b4
Logue <logue@hotmail.co.jp>
2023-12-09 03:14:53 +0900

    Update vue, element plus and vite.

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,10 +12,23 @@ import viteConfig from './vite.config';
 export default mergeConfig(
   viteConfig({ command: 'serve', mode: '' }),
   defineConfig({
+    // Resolver
+    resolve: {
+      // https://vitest.dev/config/#alias
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+      },
+      extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    },
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
+      globals: true,
       root: fileURLToPath(new URL('./', import.meta.url)),
+      deps: {
+        inline: ['element-plus'],
+      },
     },
   })
 );
